9de9e548f04cf9a5e7137a78de072296600bbb2a
Michael Lewicki <lewickiclass@gmail.com>
2024-04-15 22:51:19 -0400

    added exception handling

---
+++ src/main/java/cl/lewickidev/ganbare/msganbareapi/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,74 @@
+package cl.lewickidev.ganbare.msganbareapi.shared.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        body.put("path", request.getDescription(false));
+        //ex.printStackTrace();
+        log.info("MethodArgumentNotValidException: {}", ex.toString());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = { HandlerMethodValidationException.class })
+    public ResponseEntity<?> handleMethodValidationException(HandlerMethodValidationException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", ex.getMessage().replace("\\", "").replace("\"", ""));
+        body.put("path", request.getDescription(false));
+        ex.printStackTrace();
+        log.info("HandlerMethodValidationException: {}", ex.toString());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = { HandledException.class })
+    public ResponseEntity<?> handleHandledException(HandledException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", ex.getCode());
+        HttpStatus httpStatus = HttpStatus.valueOf(Integer.parseInt(ex.getCode()));
+        body.put("error", httpStatus.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        body.put("path", request.getDescription(false));
+        //ex.printStackTrace();
+        log.error("HandledException: {}", ex.toString());
+        return new ResponseEntity<>(body, httpStatus);
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    public ResponseEntity<?> handleException(Exception ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        body.put("error", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        body.put("path", request.getDescription(false));
+        ex.printStackTrace();
+        log.error("Exception: {}", ex.toString());
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
+
