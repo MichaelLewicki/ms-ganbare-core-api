9b3b79192db3d8dd2f6d8766b8b7e958f92f2500
Michael Lewicki <lewickiclass@gmail.com>
2024-04-18 20:57:20 -0400

    character relationship was added

--- src/main/java/cl/lewickidev/ganbare/msganbarecoreapi/domain/model/Anime.java
+++ src/main/java/cl/lewickidev/ganbare/msganbarecoreapi/domain/model/Anime.java
@@ -1,21 +1,19 @@
 package cl.lewickidev.ganbare.msganbarecoreapi.domain.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.LinkedHashSet;
 
 @Data
 public class Anime {
 
     private Long id;
-    @NotBlank(message = "Name cannot be blank")
     private String name;
-    @NotNull(message = "Year cannot be null")
     private Integer year;
-    @NotBlank(message = "Description cannot be blank")
     private String description;
     private String image;
-    //private LinkedHashSet<Character> characters = new LinkedHashSet<>();
+
+    private LinkedHashSet<Character> characters = new LinkedHashSet<>();
     //private LinkedHashSet<Episode> episodes = new LinkedHashSet<>();
     //private LinkedHashSet<Genre> genres = new LinkedHashSet<>();
 
