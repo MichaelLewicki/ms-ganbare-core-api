ed475df14d273d383b497ed48bdbb653efbfe3e8
Michael Lewicki <lewickiclass@gmail.com>
2024-04-19 22:25:54 -0400

    character relationship was added

--- src/main/java/cl/lewickidev/ganbare/msganbarecoreapi/infrastructure/adapter/output/entity/AnimeEntity.java
+++ src/main/java/cl/lewickidev/ganbare/msganbarecoreapi/infrastructure/adapter/output/entity/AnimeEntity.java
@@ -1,7 +1,13 @@
 package cl.lewickidev.ganbare.msganbarecoreapi.infrastructure.adapter.output.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -10,19 +16,26 @@ public class AnimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
+    @Column(name = "ID_ANIME")
     private Long id;
+
     @Column(name = "NAME")
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
     @Column(name = "YEAR", length = 4)
+    @NotNull(message = "Year cannot be null")
     private Integer year;
-    @Column(name = "DESCRIPTION", length = 500)
+
+    @Column(name = "DESCRIPTION", length = 900)
+    @NotBlank(message = "Description cannot be blank")
     private String description;
+
     @Column(name = "IMAGE")
     private String image;
 
-    //private LinkedHashSet<CharacterEntity> characters = new LinkedHashSet<>();
-    //private LinkedHashSet<EpisodeEntity> episodes = new LinkedHashSet<>();
-    //private LinkedHashSet<GenreEntity> genres = new LinkedHashSet<>();
+    @OneToMany(mappedBy = "anime", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<CharacterEntity> characters = new ArrayList<>();
 
 }
