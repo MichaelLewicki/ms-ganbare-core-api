1807f98449b37b2997d3f1cf365805dbdee19c93
Michael Lewicki <lewickiclass@gmail.com>
2024-04-21 04:28:29 -0400

    genre mappers were added

--- src/main/java/cl/lewickidev/ganbare/msganbarecoreapi/infrastructure/adapter/output/mapper/DomainEntityMapper.java
+++ src/main/java/cl/lewickidev/ganbare/msganbarecoreapi/infrastructure/adapter/output/mapper/DomainEntityMapper.java
@@ -3,9 +3,11 @@ package cl.lewickidev.ganbare.msganbarecoreapi.infrastructure.adapter.output.map
 import cl.lewickidev.ganbare.msganbarecoreapi.domain.model.Anime;
 import cl.lewickidev.ganbare.msganbarecoreapi.domain.model.Character;
 import cl.lewickidev.ganbare.msganbarecoreapi.domain.model.Episode;
+import cl.lewickidev.ganbare.msganbarecoreapi.domain.model.Genre;
 import cl.lewickidev.ganbare.msganbarecoreapi.infrastructure.adapter.output.entity.AnimeEntity;
 import cl.lewickidev.ganbare.msganbarecoreapi.infrastructure.adapter.output.entity.CharacterEntity;
 import cl.lewickidev.ganbare.msganbarecoreapi.infrastructure.adapter.output.entity.EpisodeEntity;
+import cl.lewickidev.ganbare.msganbarecoreapi.infrastructure.adapter.output.entity.GenreEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
@@ -41,7 +43,7 @@ public interface DomainEntityMapper {
 		return characterEntities.map(this::toDTO);
 	}
 
-	//Episodes...
+	//Episode...
 
 	Episode toDTO(EpisodeEntity entity);
 
@@ -52,6 +54,18 @@ public interface DomainEntityMapper {
 
 	default Page<Episode> toEpisodeDTOs(Page<EpisodeEntity> episodeEntities) {
 		return episodeEntities.map(this::toDTO);
+	}
+
+	//Genre...
+
+	Genre toDTO(GenreEntity entity);
+
+	GenreEntity toEntity(Genre dto);
+
+	//List<Anime> toGenreDTOs(List<AnimeEntity> animeEntities);
+
+	default Page<Genre> toGenreDTOs(Page<GenreEntity> genreEntities) {
+		return genreEntities.map(this::toDTO);
 	}
 
 }
